9d3289993cc5d43dd53e5bc37207bf11796385c2
rabbt <albertnio@live.com.mx>
2020-09-16 14:06:31 -0500

    listos

--- ProyectoDjango/settings.py
+++ ProyectoDjango/settings.py
@@ -132,17 +132,17 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 ####################################################
-import dj_database_url  
-db_from_env = dj_database_url.config(conn_max_age=500)  
-DATABASES['default'].update(db_from_env)  
+# import dj_database_url  
+# db_from_env = dj_database_url.config(conn_max_age=500)  
+# DATABASES['default'].update(db_from_env)  
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  
-STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  
+# STATIC_URL = '/static/'
 
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (  
-    os.path.join(BASE_DIR, 'static'),
-)
+# # Extra places for collectstatic to find static files.
+# STATICFILES_DIRS = (  
+#     os.path.join(BASE_DIR, 'static'),
+# )
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 ######################################################
